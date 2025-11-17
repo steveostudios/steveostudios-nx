055d737356870483cc1b43b5d607cc8a6b2c3947
steveostudios <steveostudios@gmail.com>
2025-11-17 14:03:37 -0500

    add filter

--- apps/eleventy/src/_data/projects.js
+++ apps/eleventy/src/_data/projects.js
@@ -4,21 +4,24 @@ const { getFirebaseRecords, getImage } = require("../../utils/firebase");
 module.exports = async () => {
 	const results = await getFirebaseRecords("projects");
 
-	const fixed = await results.map(async (project) => {
-		const images = await Promise.all(
-			project.images.map(async (image) => {
-				return await getImage("projects", `${project.id}/${image.filename}`)
-					.then((url) => {
-						return { ...image, url };
-					})
-					.catch((error) => {
-						console.log(error);
-					});
-			})
-		);
+	const fixed = await results
+		.filter((project) => !project.hidden)
+		.sort((a, b) => a.order - b.order)
+		.map(async (project) => {
+			const images = await Promise.all(
+				project.images.map(async (image) => {
+					return await getImage("projects", `${project.id}/${image.filename}`)
+						.then((url) => {
+							return { ...image, url };
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+				})
+			);
 
-		return { ...project, images };
-	});
+			return { ...project, images };
+		});
 
 	return await Promise.all(fixed);
 };
